82e978cfb49d145fbfa53884601d4dff20031092
axel.ulmestig@gmail.com <axel.ulmestig@gmail.com>
2018-03-03 17:36:36 +0100

    Add default values in templates

--- src/Modify.hs
+++ src/Modify.hs
@@ -3,14 +3,37 @@ module Modify (
     modify
 ) where
 
-import Data.Map             (Map, mapKeys, foldrWithKey)
+import Data.Map             (Map, mapKeys, foldrWithKey, empty, singleton, union)
 import Data.String.Utils    (replace)
+import Text.Regex.TDFA      ((=~))
+import Data.String.Utils    (strip)
 
 modify :: Map String String -> String -> String
-modify config = insertValues (addBrackets config)
+modify origConfig origTemplate = insertValues (addBrackets config) template
+    where   config      = union origConfig (getDefaults origTemplate)
+            template    = overwriteDefaults origTemplate
 
 addBrackets :: Map String String -> Map String String
 addBrackets = mapKeys (("{"++) . (++"}"))
 
 insertValues :: Map String String -> String -> String
 insertValues = flip $ foldrWithKey replace
+
+getDefaults :: String -> Map String String
+getDefaults template = mconcat
+    . map toMap
+    $ template =~ regex
+    where   toMap (_:key:defaultValue:_)    = singleton (strip key) defaultValue
+            toMap _                         = empty
+
+overwriteDefaults :: String -> String
+overwriteDefaults template = foldr ($) template
+    . map removeDefault
+    $ template =~ regex
+    where   removeDefault (expr:key:_)  = replace expr ("{" ++ (strip key) ++ "}")
+            removeDefault _             = id
+
+--The regex engine can't handle \S for some reason,
+--So I have to use trim from String.Utils instead...
+regex = "{(.*)\\| *(.*)}" :: String
+--regex = "{(.*\\S)\\|\\s*(.*)}" :: String
--- test/Spec.hs
+++ test/Spec.hs
@@ -23,7 +23,11 @@ assertions = [
         assertion3,
         assertion4,
         assertion5,
-        assertion6
+        assertion6,
+        assertion7,
+        assertion8,
+        assertion9,
+        assertion10
     ]
 
 assertion1 = assertEqual "no manifests" expected actual
@@ -56,10 +60,29 @@ assertion6 = assertEqual "manifest with array" expected actual
     where   expected    = [File "<h1> greetings frukt</h1>" "template3.manifest3.html"]
             actual      = generate [template3] [manifest3]
 
-manifest1 = File "{\"ost\": {\"fisk\": \"kex\"}}" "manifest1" :: File
+assertion7 = assertEqual "template with unnecessary default value" expected actual
+    where   expected    = [File "<h1> hello kex</h1>" "template4.manifest1.html"]
+            actual      = generate [template4] [manifest1]
+
+assertion8 = assertEqual "template with necessary default value" expected actual
+    where   expected    = [File "<h1> hello ostron</h1>" "template4.manifest3.html"]
+            actual      = generate [template4] [manifest3]
+
+assertion9 = assertEqual "template with multiple keys" expected actual
+    where   expected    = [File "<h1> hello kex</h1>\n 3.0" "template5.manifest1.html"]
+            actual      = generate [template5] [manifest1]
+
+assertion10 = assertEqual "template with empty default value" expected actual
+    where   expected    = [File "<h1> hello </h1>" "template6.manifest1.html"]
+            actual      = generate [template6] [manifest1]
+
+manifest1 = File "{\"ost\": {\"fisk\": \"kex\"}, \"frukt\": 3}" "manifest1" :: File
 manifest2 = File "{\"ost\": {\"fisk\": 1}}" "manifest2" :: File
 manifest3 = File "{\"fisk\": [\"ost\", \"frukt\"]}" "manifest3" :: File
 
 template1 = File "<h1> hello {ost.fisk}</h1>" "template1.template.html" :: File
 template2 = File "<h1> good bye {ost.fisk}</h1>" "template2.template.html" :: File
 template3 = File "<h1> greetings {fisk[1]}</h1>" "template3.template.html" :: File
+template4 = File "<h1> hello {ost.fisk | ostron}</h1>" "template4.template.html" :: File
+template5 = File "<h1> hello {ost.fisk}</h1>\n {frukt}" "template5.template.html" :: File
+template6 = File "<h1> hello {ost.hamster |}</h1>" "template6.template.html" :: File
